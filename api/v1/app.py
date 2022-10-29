b01ac255967a380abebb9f03e519f49f0a3a7b31
yeonoel <yeopevroguinoel@gmail.com>
2022-10-29 15:15:05 +0000

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" Entry point of the application."""
 
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint
 from api.v1.views.index import *
 
